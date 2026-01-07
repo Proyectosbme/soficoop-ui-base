3dc7966a3d43ea4289b0d1e6f80f96cc02cf1a41
Ivan Marroquin <bryan.marroquin@coop1.com.sv>
2026-01-07 16:07:15 -0600

    Ordenanmiento de rutas

--- src/app/ui/pages/public/public.routes.ts
+++ src/app/ui/pages/public/public.routes.ts
@@ -1,7 +1,6 @@
 // ui/pages/public/public.routes.ts
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { Notfound } from './notfound/notfound';
 
 export const PUBLIC_ROUTES: Routes = [
   { path: '', component: Home }
--- src/app/ui/pages/security/auth/auth.routes.ts
+++ src/app/ui/pages/security/auth/auth.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Access } from './access';
-import { Login } from './login';
 import { Error } from './error';
 
 export const AUTH_ROUTES: Routes = [
