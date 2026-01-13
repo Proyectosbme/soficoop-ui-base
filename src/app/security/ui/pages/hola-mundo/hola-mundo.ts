480b15fa24640abfdbfc572d21075b5a2a940590
Ivan Marroquin <bryan.marroquin@coop1.com.sv>
2026-01-13 16:06:09 -0600

    Preparado para desarollo

--- src/app/security/ui/pages/hola-mundo/hola-mundo.ts
+++ src/app/security/ui/pages/hola-mundo/hola-mundo.ts
@@ -1,11 +1,62 @@
 import { Component } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
-  selector: 'app-hola-mundo',
-  imports: [],
-  templateUrl: './hola-mundo.html',
-  styleUrl: './hola-mundo.scss',
+    selector: 'app-hola-mundo',
+    imports: [ButtonModule, ToastModule, ConfirmPopupModule],
+    templateUrl: './hola-mundo.html',
+    styleUrl: './hola-mundo.scss',
+    providers: [ConfirmationService, MessageService]
 })
 export class HolaMundo {
+    constructor(private confirmationService: ConfirmationService, private messageService: MessageService) { }
+
+    confirm1(event: Event) {
+        this.confirmationService.confirm({
+            target: event.currentTarget as EventTarget,
+            message: 'Are you sure you want to proceed?',
+            icon: 'pi pi-exclamation-triangle',
+            rejectButtonProps: {
+                label: 'Cancel',
+                severity: 'secondary',
+                outlined: true
+            },
+            acceptButtonProps: {
+                label: 'Save'
+            },
+            accept: () => {
+                this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
+            },
+            reject: () => {
+                this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+            }
+        });
+    }
+
+    confirm2(event: Event) {
+        this.confirmationService.confirm({
+            target: event.currentTarget as EventTarget,
+            message: 'Do you want to delete this record?',
+            icon: 'pi pi-info-circle',
+            rejectButtonProps: {
+                label: 'Cancel',
+                severity: 'secondary',
+                outlined: true
+            },
+            acceptButtonProps: {
+                label: 'Delete',
+                severity: 'danger'
+            },
+            accept: () => {
+                this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'Record deleted', life: 3000 });
+            },
+            reject: () => {
+                this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+            }
+        });
+    }
 
 }
